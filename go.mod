c894e1074e76be2694d7315a923e01e134fb06cf
ligq <lizaza@lizaza.cn>
2023-08-23 10:28:01 +0800

    修改传入参数类型

--- go.mod
+++ go.mod
@@ -3,6 +3,7 @@ module github.com/lizazacn/ssh2x11
 go 1.19
 
 require (
+	github.com/gorilla/websocket v1.5.0 // indirect
 	golang.org/x/crypto v0.12.0 // indirect
 	golang.org/x/sys v0.11.0 // indirect
 )
--- x11.go
+++ x11.go
@@ -3,11 +3,13 @@ package ssh2x11
 import (
 	"encoding/hex"
 	"errors"
+	"github.com/gorilla/websocket"
 	"golang.org/x/crypto/ssh"
 	"io"
 	"log"
 	"math/rand"
 	"net"
+	"os"
 	"runtime"
 	"sync"
 )
@@ -20,7 +22,7 @@ type X11Request struct {
 	ScreenNumber     uint32 // 屏幕编号
 }
 
-func NewX11Session(client *ssh.Client, conn net.Conn) (*ssh.Session, chan error) {
+func NewX11Session(client *ssh.Client, conn interface{}) (*ssh.Session, chan error) {
 	var x11Request = new(X11Request)
 	x11Request.AuthProtocol = AUTH_MIT_MAGIC_COOKIE_1
 	x11Request.AuthCookie = newCookie(16)
@@ -29,7 +31,7 @@ func NewX11Session(client *ssh.Client, conn net.Conn) (*ssh.Session, chan error)
 	return CreateX11Session(client, x11Request, conn)
 }
 
-func CreateX11Session(client *ssh.Client, x11Request *X11Request, conn net.Conn) (*ssh.Session, chan error) {
+func CreateX11Session(client *ssh.Client, x11Request *X11Request, conn interface{}) (*ssh.Session, chan error) {
 	var errChan = make(chan error, 10)
 	session, err := client.NewSession()
 	if err != nil {
@@ -89,13 +91,31 @@ func newCookie(length int) string {
 	return hex.EncodeToString(randomBytes)
 }
 
-func forwardToLocal(channel ssh.Channel, conn net.Conn) error {
-
+func forwardToLocal(channel ssh.Channel, conn interface{}) error {
 	if channel == nil {
 		return errors.New("sshChannel is not exits")
 	}
-
 	var errChan = make(chan error, 4)
+	switch conn.(type) {
+	case net.Conn:
+		NetConnForward(conn.(net.Conn), channel, errChan)
+	case *websocket.Conn:
+		WsConnForward(conn.(*websocket.Conn), channel, errChan)
+	case *os.File:
+		FileForward(conn.(*os.File), channel, errChan)
+	default:
+		return errors.New("conn类型不在许可范围！")
+	}
+	select {
+	case err := <-errChan:
+		return err
+	default:
+		return nil
+	}
+}
+
+// NetConnForward 转发到NetConn
+func NetConnForward(conn net.Conn, channel ssh.Channel, errChan chan error) {
 	var wait sync.WaitGroup
 	wait.Add(2)
 	go func() {
@@ -113,7 +133,6 @@ func forwardToLocal(channel ssh.Channel, conn net.Conn) error {
 	go func() {
 		defer func(channel ssh.Channel) {
 			errChan <- channel.CloseWrite()
-
 		}(channel)
 		defer wait.Done()
 		_, err := io.Copy(channel, conn)
@@ -123,10 +142,73 @@ func forwardToLocal(channel ssh.Channel, conn net.Conn) error {
 		}
 	}()
 	wait.Wait()
-	select {
-	case err := <-errChan:
-		return err
-	default:
-		return nil
+}
+
+// WsConnForward 转发到WSConn
+func WsConnForward(conn *websocket.Conn, channel ssh.Channel, errChan chan error) {
+	_, reader, err := conn.NextReader()
+	if err != nil {
+		return
 	}
+	writer, err := conn.NextWriter(websocket.BinaryMessage)
+	if err != nil {
+		return
+	}
+	var wait sync.WaitGroup
+	wait.Add(2)
+	go func() {
+		defer func(conn *websocket.Conn) {
+			errChan <- conn.Close()
+		}(conn)
+		defer wait.Done()
+		_, err = io.Copy(writer, channel)
+		if err != nil {
+			errChan <- err
+			return
+		}
+	}()
+
+	go func() {
+		defer func(channel ssh.Channel) {
+			errChan <- channel.CloseWrite()
+		}(channel)
+		defer wait.Done()
+		_, err = io.Copy(channel, reader)
+		if err != nil {
+			errChan <- err
+			return
+		}
+	}()
+	wait.Wait()
+}
+
+// FileForward 转发到文件
+func FileForward(file *os.File, channel ssh.Channel, errChan chan error) {
+
+	var wait sync.WaitGroup
+	wait.Add(2)
+	go func() {
+		defer func(file *os.File) {
+			errChan <- file.Close()
+		}(file)
+		defer wait.Done()
+		_, err := io.Copy(file, channel)
+		if err != nil {
+			errChan <- err
+			return
+		}
+	}()
+
+	go func() {
+		defer func(channel ssh.Channel) {
+			errChan <- channel.CloseWrite()
+		}(channel)
+		defer wait.Done()
+		_, err := io.Copy(channel, file)
+		if err != nil {
+			errChan <- err
+			return
+		}
+	}()
+	wait.Wait()
 }
